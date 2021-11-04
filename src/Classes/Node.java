3173fecd8cf8fae2db6bbe8f0114d7b100783666
Jacob Dong <jjd180002@utdallas.edu>
2021-11-04 10:06:31 -0500

    added classes

---
+++ src/Classes/Node.java
@@ -0,0 +1,39 @@
+package BinaryTree;
+
+public class Node implements Comparable<Node>
+{
+    int num;
+    Node left = null, right = null;
+    
+    Node(int n)
+    { num = n;      }
+    
+    Node(char c)
+    { op = c;      }
+    
+    public int getNum()
+    { return num;   }
+    
+    public void setNum(int n)
+    {  num = n;  }
+
+    public Node getLeft()
+    { return left;  }
+    
+    public void setLeft(Node n)
+    {  left = n;    }
+    
+    public Node getRight()
+    { return right;  }
+    
+    public void setRight(Node n)
+    {  right = n;    }
+    
+    @Override
+    public int compareTo(Node n)
+    {
+        return this.num - n.num;
+    }
+    
+    
+}
