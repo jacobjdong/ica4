0c9fc6496b714201d63a916bcccfea1fbacc4f61
santhoshkaran63 <93723082+santhoshkaran63@users.noreply.github.com>
2021-11-04 10:34:49 -0500

    changes to queue class

--- src/Classes/QueueICA.java
+++ src/Classes/QueueICA.java
@@ -1,6 +1,27 @@
-package Classes;
 import java.util.*;
 
-public class QueueICA implements Queue {
-    
+public class QueueICA implements Queue{
+    Queue<Node<E>> queue = new BinaryTree<Node<E>>();
+
+if(root != null)
+    {
+        queue.add(root);
+    }
+
+while(!queue.isEmpty())
+    {
+        TreeNode<E> temp = queue.remove();
+
+        System.out.println(temp);
+
+        if(temp.left != null)
+        {
+            queue.add(temp.left);
+        }
+        if(temp.right != null)
+        {
+            queue.add(temp.right);
+        }
+    }
 }
+
